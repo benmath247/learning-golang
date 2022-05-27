b978f9ab6aee9b61aa20218dcd3ddaaa5e0b3da6
Ben Math <benjamin.aaron.math@gmail.com>
2022-05-27 19:25:38 -0400

    formating strings

--- main.go
+++ main.go
@@ -3,7 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	var conferenceName = "Go Conference"
+	var conferenceTickets = 50
+	var remainingTickets = 50
+	// fmt.Println("Welcome to", conferenceName, "booking application")
+	fmt.Printf("Welcome to %v booking application", conferenceName)
+	fmt.Println("We have", conferenceTickets, "tickets and", remainingTickets, "are still available")
+	fmt.Println("Get your tickets here to attend")
+
+	conferenceTickets = 30
+
+	var userName string
+	var userTickets int
+	// ask for their name
+	userName = "Tom"
+
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
 }
 
 func main2() {
